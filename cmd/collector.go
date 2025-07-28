8128b61b4f1657c7e5824c586e1f10874c0e90d3
ftalpo <francesco.talpo@e4company.com>
2025-07-28 15:58:09 +0000

    Change factory func names for clarity

--- cmd/collector.go
+++ cmd/collector.go
@@ -33,7 +33,7 @@ type ProviderFactory struct {
 	defaultLabels []string
 }
 
-func (f *ProviderFactory) NewLogMetricProvider(
+func (f *ProviderFactory) CreateLogMetricProvider(
 	fqName string,
 	help string,
 	jsonKey string,
@@ -50,7 +50,7 @@ func (f *ProviderFactory) NewLogMetricProvider(
 	)
 }
 
-func (f *ProviderFactory) NewInfoMetricProvider(
+func (f *ProviderFactory) CreateInfoMetricProvider(
 	fqName string,
 	help string,
 	jsonKey string,
@@ -84,31 +84,31 @@ func newNvmeCollector(ocpEnabled bool) prometheus.Collector {
 
 	// Info metrics
 	infoMetricProviders := []pkg.InfoMetricProvider{
-		gaugeValueFactory.NewInfoMetricProvider(
+		gaugeValueFactory.CreateInfoMetricProvider(
 			"nvme_namespace",
 			"",
 			"NameSpace",
 			infoLabels,
 		),
-		gaugeValueFactory.NewInfoMetricProvider(
+		gaugeValueFactory.CreateInfoMetricProvider(
 			"nvme_used_bytes",
 			"",
 			"UsedBytes",
 			infoLabels,
 		),
-		gaugeValueFactory.NewInfoMetricProvider(
+		gaugeValueFactory.CreateInfoMetricProvider(
 			"nvme_maximum_lba",
 			"",
 			"MaximumLBA",
 			infoLabels,
 		),
-		gaugeValueFactory.NewInfoMetricProvider(
+		gaugeValueFactory.CreateInfoMetricProvider(
 			"nvme_physical_size",
 			"",
 			"PhysicalSize",
 			infoLabels,
 		),
-		gaugeValueFactory.NewInfoMetricProvider(
+		gaugeValueFactory.CreateInfoMetricProvider(
 			"nvme_sector_size",
 			"",
 			"SectorSize",
@@ -118,128 +118,128 @@ func newNvmeCollector(ocpEnabled bool) prometheus.Collector {
 
 	// Smart-log metrics
 	logMetricProviders := []pkg.LogMetricProvider{
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_critical_warning",
 			"Critical warnings for the state of the controller",
 			"critical_warning",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_temperature",
 			"Temperature in degrees fahrenheit",
 			"temperature",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_avail_spare",
 			"Normalized percentage of remaining spare capacity available",
 			"avail_spare",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_spare_thresh",
 			"Async event completion may occur when avail spare < threshold",
 			"spare_thresh",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_percent_used",
 			"Vendor specific estimate of the percentage of life used",
 			"percent_used",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_endurance_grp_critical_warning_summary",
 			"Critical warnings for the state of endurance groups",
 			"endurance_grp_critical_warning_summary",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_data_units_read",
 			"Number of 512 byte data units host has read",
 			"data_units_read",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_data_units_written",
 			"Number of 512 byte data units the host has written",
 			"data_units_written",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_host_read_commands",
 			"Number of read commands completed",
 			"host_read_commands",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_host_write_commands",
 			"Number of write commands completed",
 			"host_write_commands",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_controller_busy_time",
 			"Amount of time in minutes controller busy with IO commands",
 			"controller_busy_time",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_power_cycles",
 			"Number of power cycles",
 			"power_cycles",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_power_on_hours",
 			"Number of power on hours",
 			"power_on_hours",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_unsafe_shutdowns",
 			"Number of unsafe shutdowns",
 			"unsafe_shutdowns",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_media_errors",
 			"Number of unrecovered data integrity errors",
 			"media_errors",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_num_err_log_entries",
 			"Lifetime number of error log entries",
 			"num_err_log_entries",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_warning_temp_time",
 			"Amount of time in minutes temperature > warning threshold",
 			"warning_temp_time",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_critical_comp_time",
 			"Amount of time in minutes temperature > critical threshold",
 			"critical_comp_time",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_thm_temp1_trans_count",
 			"Number of times controller transitioned to lower power",
 			"thm_temp1_trans_count",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_thm_temp2_trans_count",
 			"Number of times controller transitioned to lower power",
 			"thm_temp2_trans_count",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_thm_temp1_trans_time",
 			"Total number of seconds controller transitioned to lower power",
 			"thm_temp1_total_time",
 		),
 
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_thm_temp2_trans_time",
 			"Total number of seconds controller transitioned to lower power",
 			"thm_temp2_total_time",
@@ -248,196 +248,198 @@ func newNvmeCollector(ocpEnabled bool) prometheus.Collector {
 
 	// OCP smart-log metrics
 	ocpLogMetricProviders := []pkg.LogMetricProvider{
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_physical_media_units_written_hi",
 			"Physical meda units written high",
 			"Physical media units written.hi",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_physical_media_units_written_lo",
 			"Physical meda units written low",
 			"Physical media units written.lo",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_physical_media_units_read_hi",
 			"Physical meda units read high",
 			"Physical media units read.hi",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_physical_media_units_read_lo",
 			"Physical meda units read low",
 			"Physical media units read.lo",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_bad_user_nand_blocks_raw",
 			"",
 			"Bad user nand blocks - Raw",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_bad_user_nand_blocks_normalized",
 			"",
 			"Bad user nand blocks - Normalized",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_bad_system_nand_blocks_raw",
 			"",
 			"Bad system nand blocks - Raw",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_bad_system_nand_blocks_normalized",
 			"",
 			"Bad system nand blocks - Normalized",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_xor_recovery_count",
 			"",
 			"XOR recovery count",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_uncorrectable_uead_error_count",
 			"",
 			"Uncorrectable read error count",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_soft_ecc_error_count",
 			"",
 			"Soft ecc error count",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_end_to_end_detected_errors",
 			"",
 			"End to end detected errors",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_end_to_end_corrected_errors",
 			"",
 			"End to end corrected errors",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_system_data_percent_used",
 			"",
 			"System data percent used",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_refresh_counts",
 			"",
 			"Refresh counts",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_max_user_data_erase_counts",
 			"",
 			"Max User data erase counts",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_min_user_data_erase_counts",
 			"",
 			"Min User data erase counts",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_number_of_thermal_throttling_events",
 			"",
 			"Number of Thermal throttling events",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_current_throttling_status",
 			"",
 			"Current throttling status",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_pcie_correctable_error_count",
 			"",
 			"PCIe correctable error count",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_incomplete_shutdowns",
 			"",
 			"Incomplete shutdowns",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_percent_free_blocks",
 			"",
 			"Percent free blocks",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_capacitor_health",
 			"",
 			"Capacitor health",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_unaligned_io",
 			"",
 			"Unaligned I/O",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_security_version_number",
 			"",
 			"Security Version Number",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_nuse_namespace_utilization",
 			"",
 			"NUSE - Namespace utilization",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_plp_start_count",
 			"",
 			"PLP start count",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_endurance_estimate",
 			"",
 			"Endurance estimate",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_log_page_version",
 			"",
 			"Log page version",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_log_page_guid",
 			"",
 			"Log page GUID",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_errata_version_field",
 			"",
 			"Errata Version Field",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_point_version_field",
 			"",
 			"Point Version Field",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_minor_version_field",
 			"",
 			"Minor Version Field",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_major_version_field",
 			"",
 			"Major Version Field",
 		),
-		gaugeValueFactory.NewLogMetricProvider(
+		gaugeValueFactory.CreateLogMetricProvider(
 			"nvme_nvme_errata_version",
 			"",
 			"NVMe Errata Version",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_pcie_link_retraining_count",
 			"",
 			"PCIe Link Retraining Count",
 		),
-		counterValueFactory.NewLogMetricProvider(
+		counterValueFactory.CreateLogMetricProvider(
 			"nvme_power_state_change_count",
 			"",
 			"Power State Change Count",
 		),
 	}
 
-	var collectors []pkg.MetricCollector
-	collectors = append(collectors, pkg.NewInfoMetricCollector(infoMetricProviders))
-	collectors = append(collectors, pkg.NewLogMetricCollector(logMetricProviders, getSmartLogData))
+	// the info and smart-log collectors are always present
+	collectors := []pkg.MetricCollector{
+		pkg.NewInfoMetricCollector(infoMetricProviders),
+		pkg.NewLogMetricCollector(logMetricProviders, getSmartLogData),
+	}
 
 	if ocpEnabled {
 		collectors = append(collectors, pkg.NewLogMetricCollector(ocpLogMetricProviders, getOcpSmartLogData))
